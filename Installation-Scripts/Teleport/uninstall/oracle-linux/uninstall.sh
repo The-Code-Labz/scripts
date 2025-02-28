9298d316e6a0a9c7a79f3e5a26319b6de63e061f
Davonte <dlynton01@gmail.com>
2025-02-28 03:44:16 +0000

    Update file uninstall.sh

--- Installation-Scripts/Teleport/uninstall/oracle-linux/uninstall.sh
+++ Installation-Scripts/Teleport/uninstall/oracle-linux/uninstall.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-# Check if the script is run as root, if not use sudo
-if [ "$EUID" -ne 0 ]; then
-    SUDO="sudo"
-else
-    SUDO=""
-fi
-
 # Stop Teleport service if running
 if systemctl is-active --quiet teleport; then
     echo "Stopping Teleport service..."
-    $SUDO systemctl stop teleport
+    systemctl stop teleport
 else
     echo "Teleport service is not running."
 fi
@@ -18,7 +11,7 @@ fi
 # Kill any running Teleport processes
 if pgrep -f teleport > /dev/null; then
     echo "Killing Teleport processes..."
-    $SUDO pkill -f teleport
+    pkill -f teleport
 else
     echo "No running Teleport processes found."
 fi
@@ -26,7 +19,7 @@ fi
 # Remove Teleport data directory
 if [ -d /var/lib/teleport ]; then
     echo "Removing /var/lib/teleport..."
-    $SUDO rm -rf /var/lib/teleport
+    rm -rf /var/lib/teleport
 else
     echo "No data directory found at /var/lib/teleport."
 fi
@@ -34,7 +27,7 @@ fi
 # Remove Teleport configuration file
 if [ -f /etc/teleport.yaml ]; then
     echo "Removing /etc/teleport.yaml..."
-    $SUDO rm -f /etc/teleport.yaml
+    rm -f /etc/teleport.yaml
 else
     echo "No configuration file found at /etc/teleport.yaml."
 fi
@@ -42,7 +35,7 @@ fi
 # Uninstall Teleport package
 if yum list installed teleport &>/dev/null; then
     echo "Uninstalling Teleport package..."
-    $SUDO yum remove -y teleport
+    yum remove -y teleport
 else
     echo "Teleport package is not installed."
 fi
