3eac03b018a34348385f406aa57a57aadb5f121b
Davonte <dlynton01@gmail.com>
2025-03-01 15:20:03 +0000

    Update file user.sh

--- Ubuntu Scripts/Add User/user.sh
+++ Ubuntu Scripts/Add User/user.sh
@@ -1,31 +1,22 @@
 #!/bin/bash
+set -e
 
-set -e  # Exit on error
+USERNAME=$1
+PASSWORD=$2
+SUDO_PRIV=$3
 
-# Check if script is run as root
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 
-   exit 1
+if [[ -z "$USERNAME" || -z "$PASSWORD" ]]; then
+    echo "Usage: $0 <username> <password> [sudo]"
+    exit 1
 fi
 
-# Read username as input
-echo -n "Enter the new username: "
-read username
+echo "Creating user $USERNAME..."
+sudo adduser --gecos "" --disabled-password "$USERNAME"
+echo "$USERNAME:$PASSWORD" | sudo chpasswd
 
-# Create the new user
-echo "Creating user $username..."
-sudo adduser "$username"
-
-# Set password for the new user
-echo "Setting password for $username..."
-sudo passwd "$username"
-
-# Add user to sudo group (optional)
-echo -n "Should the user have sudo privileges? (y/n): "
-read give_sudo
-if [[ "$give_sudo" == "y" ]]; then
-    sudo usermod -aG sudo "$username"
-    echo "$username has been granted sudo privileges."
+if [[ "$SUDO_PRIV" == "yes" ]]; then
+    sudo usermod -aG sudo "$USERNAME"
+    echo "$USERNAME has been granted sudo privileges."
 fi
 
-echo "User $username created successfully."
+echo "User $USERNAME created successfully."
